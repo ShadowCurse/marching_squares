f26c3dd435379e77eab44fe3d3a62fc9f2bac897
ShadowCurse <shadowcurse57@gmail.com>
2021-12-14 15:29:04 +0300

    added perlin noise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+mod perlin;
+use perlin::Perlin;
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
@@ -10,26 +13,45 @@ fn main() {
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    let sprite_size = 5.0;
-    let half_grid_size = 8;
+    let sprite_size = 3.0;
+    let half_grid_size = 2;
+
+    let perlin = Perlin::new();
 
     for x in -half_grid_size..half_grid_size {
         for y in -half_grid_size..half_grid_size {
-            let translation = (Vec3::new(x as f32, y as f32, 0.0)
-                + Vec3::new(
-                    half_grid_size as f32 / 4.0,
-                    half_grid_size as f32 / 4.0,
-                    0.0,
-                ))
-                * sprite_size
-                * 10.0;
-            // println!("adding sprite with translation: {:#?}", translation);
+            let translation =
+                (Vec3::new(x as f32, y as f32, 0.0) + Vec3::new(0.5, 0.5, 0.0)) * sprite_size * 2.0;
+            let color = Color::rgb(1.0, 1.0, 1.0)
+                * 0.5
+                * (1.0
+                    + (10.0
+                        * perlin.turb(
+                            &Vec3::new(
+                                x as f32 / half_grid_size as f32,
+                                y as f32 / half_grid_size as f32,
+                                0.0,
+                            ),
+                            2,
+                        ))
+                    .sin());
+            println!(
+                "Adding point with translation: {:?}, color: {:?}",
+                translation, color
+            );
+
             commands.spawn_bundle(SpriteBundle {
-                material: materials.add(Color::GREEN.into()),
+                material: materials.add(color.into()),
                 sprite: Sprite::new(Vec2::new(sprite_size, sprite_size)),
                 transform: Transform::from_translation(translation),
                 ..Default::default()
             });
         }
     }
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::BLUE.into()),
+        sprite: Sprite::new(Vec2::new(sprite_size, sprite_size)),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+        ..Default::default()
+    });
 }
---
+++ src/perlin.rs
@@ -0,0 +1,104 @@
+// use crate::vec3::{Point3, Vec3};
+use bevy::math::Vec3;
+type Point3 = Vec3;
+
+const PERLIN_POINT_COUNT: u32 = 256;
+
+pub struct Perlin {
+    random_vec: Vec<Vec3>,
+    perm_x: Vec<u32>,
+    perm_y: Vec<u32>,
+    perm_z: Vec<u32>,
+}
+
+impl Perlin {
+    pub fn new() -> Self {
+        use rand::distributions::Distribution;
+        let mut rng = rand::thread_rng();
+        let uniform = rand::distributions::Uniform::<f32>::new(-1.0, 1.0);
+        Self {
+            random_vec: (0..PERLIN_POINT_COUNT)
+                .map(|_| {
+                    Vec3::new(
+                        uniform.sample(&mut rng),
+                        uniform.sample(&mut rng),
+                        uniform.sample(&mut rng),
+                    )
+                    .normalize()
+                })
+                .collect::<Vec<_>>(),
+            perm_x: Self::perlin_generate_perm(),
+            perm_y: Self::perlin_generate_perm(),
+            perm_z: Self::perlin_generate_perm(),
+        }
+    }
+
+    pub fn noise(&self, point: &Point3) -> f32 {
+        let u = point.x - point.x.floor();
+        let v = point.y - point.y.floor();
+        let w = point.z - point.z.floor();
+
+        let i = point.x.floor() as i32;
+        let j = point.y.floor() as i32;
+        let k = point.z.floor() as i32;
+
+        let mut c = vec![vec![vec![Vec3::default(); 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.random_vec[(self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize])
+                        as usize]
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
+    }
+
+    pub fn turb(&self, point: &Point3, depth: u32) -> f32 {
+        let mut accum = 0.0;
+        let mut tmp_p = point.clone();
+        let mut weight = 1.0;
+        for _ in 0..depth {
+            accum += weight * Self::noise(self, &tmp_p);
+            weight *= 0.5;
+            tmp_p *= 2.0;
+        }
+        accum.abs()
+    }
+
+    fn perlin_generate_perm() -> Vec<u32> {
+        let mut p = (0..PERLIN_POINT_COUNT).map(|x| x).collect::<Vec<u32>>();
+
+        use rand::distributions::Distribution;
+        let mut rng = rand::thread_rng();
+        for i in (1..PERLIN_POINT_COUNT).rev() {
+            let uniform = rand::distributions::Uniform::new(0, i);
+            let target = uniform.sample(&mut rng);
+            p.swap(i as usize, target as usize);
+        }
+        p
+    }
+
+    fn trilinear_interp(c: &Vec<Vec<Vec<Vec3>>>, u: f32, v: f32, w: f32) -> f32 {
+        let uu = u.powi(2) * (3.0 - 2.0 * u);
+        let vv = v.powi(2) * (3.0 - 2.0 * v);
+        let ww = w.powi(2) * (3.0 - 2.0 * w);
+
+        let mut accum: f32 = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let weight = Vec3::new(u - i as f32, v - j as f32, w - k as f32);
+                    accum += (i as f32 * uu + (1.0 - i as f32) * (1.0 - uu))
+                        * (j as f32 * vv + (1.0 - j as f32) * (1.0 - vv))
+                        * (k as f32 * ww + (1.0 - k as f32) * (1.0 - ww))
+                        * c[i][j][k].dot(weight);
+                }
+            }
+        }
+        accum
+    }
+}
