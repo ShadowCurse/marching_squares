42cdc7c112dad07c4aad42452563bed58bd5e978
ShadowCurse <shadowcurse57@gmail.com>
2021-12-22 20:57:28 +0300

    added layers to the grid

--- src/grid.rs
+++ src/grid.rs
@@ -12,84 +12,115 @@ pub fn setup(
     mut standart_materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let spacing = 5.0;
-    let grid_size = 200;
-    let threshold = 0.1;
+    let width = 200;
+    let height = 200;
 
-    let h = meshes.add(Mesh::new(PrimitiveTopology::TriangleList));
-    let grid = Grid::new(grid_size, spacing, threshold, h.clone());
+    let grid = Grid::new(width, height, spacing);
     commands.insert_resource(grid);
-    commands.spawn_bundle(PbrBundle {
-        material: standart_materials.add(Color::GREEN.into()),
-        mesh: h,
-        ..Default::default()
-    });
+
+    let thresholds = [0.2, 0.1, 0.05];
+    let colors = [Color::ORANGE, Color::GREEN, Color::BLUE];
+
+    for (i, (t, c)) in thresholds
+        .into_iter()
+        .zip(colors.into_iter())
+        .rev()
+        .enumerate()
+    {
+        commands
+            .spawn_bundle(PbrBundle {
+                material: standart_materials.add(c.into()),
+                mesh: meshes.add(Mesh::new(PrimitiveTopology::TriangleList)),
+                transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0 * i as f32)),
+                ..Default::default()
+            })
+            .insert(GridLayer {
+                threshold: t,
+                values_normalize: vec![false; (width * height) as usize],
+            });
+    }
 }
 
 pub fn update_mesh(
-    mut grid: ResMut<Grid>,
     mut meshes: ResMut<Assets<Mesh>>,
-    q: Query<(&Position, &Radius), With<Ball>>,
+    mut grid: ResMut<Grid>,
+    balls: Query<(&Position, &Radius), With<Ball>>,
+    mut layers: Query<(&mut GridLayer, &Handle<Mesh>)>,
 ) {
-    if let Some(m) = meshes.get_mut(&grid.mesh) {
-        *m = grid.create_mesh(&|x, y| {
-            q.iter()
-                .fold(0.0, |sum, (p, r)| sum + Ball::calc(&p.pos, r.r, x, y))
-        });
+    grid.update(&|x, y| {
+        balls
+            .iter()
+            .fold(0.0, |sum, (p, r)| sum + Ball::calc(&p.pos, r.r, x, y))
+    });
+    for (mut l, h) in layers.iter_mut() {
+        l.update(&grid, h.clone(), &mut meshes);
     }
 }
 
-pub struct Grid {
-    pub mesh: Handle<Mesh>,
-    pub size: u32,
+pub struct GridLayer {
+    // pub mesh: Handle<Mesh>,
     pub threshold: f32,
-    pub positions: Vec<Vec3>,
-    pub values: Vec<f32>,
     pub values_normalize: Vec<bool>,
 }
 
-impl Grid {
-    pub fn new(size: u32, spacing: f32, threshold: f32, mesh: Handle<Mesh>) -> Self {
-        let half_size = (size / 2) as i32;
+pub struct Grid {
+    pub width: u32,
+    pub height: u32,
+    pub spacing: f32,
+    pub positions: Vec<Vec3>,
+    pub values: Vec<f32>,
+}
 
-        let mut positions = Vec::with_capacity(size.pow(2) as usize);
-        for y in (-half_size..half_size).rev() {
-            for x in -half_size..half_size {
+impl Grid {
+    pub fn new(width: u32, height: u32, spacing: f32) -> Self {
+        let total_points = width * height;
+        let mut positions = Vec::with_capacity(total_points as usize);
+        let half_width = (width / 2) as i32;
+        let half_height = (height / 2) as i32;
+        for y in (-half_height..half_height).rev() {
+            for x in -half_width..half_width {
                 let position =
                     (Vec3::new(x as f32, y as f32, 0.0) + Vec3::new(0.5, 0.5, 0.0)) * spacing;
-
                 positions.push(position);
             }
         }
-
-        let values = vec![0.0; size.pow(2) as usize];
-        let values_normalize = vec![false; size.pow(2) as usize];
+        let values = vec![0.0; total_points as usize];
 
         Self {
-            mesh,
-            size,
-            threshold,
+            width,
+            height,
+            spacing,
             positions,
             values,
-            values_normalize,
         }
     }
 
-    pub fn create_mesh(&mut self, f: &impl Fn(f32, f32) -> f32) -> Mesh {
+    pub fn update(&mut self, f: &impl Fn(f32, f32) -> f32) {
         for (i, pos) in self.positions.iter().enumerate() {
-            let val = f(pos[0], pos[1]);
-            self.values[i] = val;
-            self.values_normalize[i] = val > self.threshold;
+            self.values[i] = f(pos[0], pos[1]);
+        }
+    }
+}
+impl GridLayer {
+    pub fn update(
+        &mut self,
+        grid: &Grid,
+        mesh_handle: Handle<Mesh>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+    ) {
+        for (i, val) in grid.values.iter().enumerate() {
+            self.values_normalize[i] = val > &self.threshold;
         }
 
         let mut vertices = vec![];
         let mut indices = vec![];
 
-        for i in 0..(self.size - 1) {
-            for j in 0..(self.size - 1) {
-                let a = (j + i * self.size) as usize;
-                let b = (j + i * self.size + 1) as usize;
-                let c = (j + (i + 1) * self.size + 1) as usize;
-                let d = (j + (i + 1) * self.size) as usize;
+        for i in 0..(grid.height - 1) {
+            for j in 0..(grid.width - 1) {
+                let a = (j + i * grid.width) as usize;
+                let b = (j + i * grid.width + 1) as usize;
+                let c = (j + (i + 1) * grid.width + 1) as usize;
+                let d = (j + (i + 1) * grid.width) as usize;
 
                 let a_val = self.values_normalize[a];
                 let b_val = self.values_normalize[b];
@@ -105,53 +136,53 @@ impl Grid {
                 match iso_value {
                     0 => {}
                     1 => {
-                        self.corner(&mut vertices, &mut indices, c, d, a);
+                        self.corner(grid, &mut vertices, &mut indices, c, d, a);
                     }
                     2 => {
-                        self.corner(&mut vertices, &mut indices, b, c, d);
+                        self.corner(grid, &mut vertices, &mut indices, b, c, d);
                     }
                     4 => {
-                        self.corner(&mut vertices, &mut indices, a, b, c);
+                        self.corner(grid, &mut vertices, &mut indices, a, b, c);
                     }
                     8 => {
-                        self.corner(&mut vertices, &mut indices, d, a, b);
+                        self.corner(grid, &mut vertices, &mut indices, d, a, b);
                     }
 
                     7 => {
-                        self.no_corner(&mut vertices, &mut indices, a, b, c, d);
+                        self.no_corner(grid, &mut vertices, &mut indices, a, b, c, d);
                     }
                     11 => {
-                        self.no_corner(&mut vertices, &mut indices, b, c, d, a);
+                        self.no_corner(grid, &mut vertices, &mut indices, b, c, d, a);
                     }
                     13 => {
-                        self.no_corner(&mut vertices, &mut indices, c, d, a, b);
+                        self.no_corner(grid, &mut vertices, &mut indices, c, d, a, b);
                     }
                     14 => {
-                        self.no_corner(&mut vertices, &mut indices, d, a, b, c);
+                        self.no_corner(grid, &mut vertices, &mut indices, d, a, b, c);
                     }
 
                     3 => {
-                        self.split(&mut vertices, &mut indices, a, b, c, d);
+                        self.split(grid, &mut vertices, &mut indices, a, b, c, d);
                     }
                     6 => {
-                        self.split(&mut vertices, &mut indices, d, a, b, c);
+                        self.split(grid, &mut vertices, &mut indices, d, a, b, c);
                     }
                     9 => {
-                        self.split(&mut vertices, &mut indices, b, c, d, a);
+                        self.split(grid, &mut vertices, &mut indices, b, c, d, a);
                     }
                     12 => {
-                        self.split(&mut vertices, &mut indices, c, d, a, b);
+                        self.split(grid, &mut vertices, &mut indices, c, d, a, b);
                     }
 
                     5 => {
-                        self.diagonal(&mut vertices, &mut indices, a, b, c, d);
+                        self.diagonal(grid, &mut vertices, &mut indices, a, b, c, d);
                     }
                     10 => {
-                        self.diagonal(&mut vertices, &mut indices, b, c, d, a);
+                        self.diagonal(grid, &mut vertices, &mut indices, b, c, d, a);
                     }
 
                     15 => {
-                        self.square(&mut vertices, &mut indices, a, b, c, d);
+                        self.square(grid, &mut vertices, &mut indices, a, b, c, d);
                     }
 
                     _ => unreachable!(),
@@ -160,37 +191,31 @@ impl Grid {
         }
 
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-        mesh.set_attribute(
-            Mesh::ATTRIBUTE_POSITION,
-            vertices,
-        );
-        mesh.set_attribute(
-            Mesh::ATTRIBUTE_NORMAL,
-            vec![[0.0, 0.0, 1.0]; indices.len()],
-        );
-        mesh.set_attribute(
-            Mesh::ATTRIBUTE_UV_0,
-            vec![[0.0, 0.0]; indices.len()],
-        );
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0.0, 0.0, 1.0]; indices.len()]);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0.0, 0.0]; indices.len()]);
         mesh.set_indices(Some(Indices::U32(indices)));
-        mesh
+        if let Some(m) = meshes.get_mut(&mesh_handle) {
+            *m = mesh;
+        }
     }
 
     fn corner(
         &self,
+        grid: &Grid,
         vertices: &mut Vec<[f32; 3]>,
         indices: &mut Vec<u32>,
         p1: usize,
         p2: usize,
         p3: usize,
     ) {
-        let pos_1 = self.positions[p1];
-        let pos_2 = self.positions[p2];
-        let pos_3 = self.positions[p3];
+        let pos_1 = grid.positions[p1];
+        let pos_2 = grid.positions[p2];
+        let pos_3 = grid.positions[p3];
 
-        let val_1 = self.values[p1];
-        let val_2 = self.values[p2];
-        let val_3 = self.values[p3];
+        let val_1 = grid.values[p1];
+        let val_2 = grid.values[p2];
+        let val_3 = grid.values[p3];
 
         let intersection_1 = pos_1.lerp(pos_2, val_2 / (val_1 + val_2));
         let intersection_2 = pos_2.lerp(pos_3, val_3 / (val_2 + val_3));
@@ -206,6 +231,7 @@ impl Grid {
 
     fn no_corner(
         &self,
+        grid: &Grid,
         vertices: &mut Vec<[f32; 3]>,
         indices: &mut Vec<u32>,
         p1: usize,
@@ -213,14 +239,14 @@ impl Grid {
         p3: usize,
         p4: usize,
     ) {
-        let pos_1 = self.positions[p1];
-        let pos_2 = self.positions[p2];
-        let pos_3 = self.positions[p3];
-        let pos_4 = self.positions[p4];
+        let pos_1 = grid.positions[p1];
+        let pos_2 = grid.positions[p2];
+        let pos_3 = grid.positions[p3];
+        let pos_4 = grid.positions[p4];
 
-        let val_1 = self.values[p1];
-        let val_2 = self.values[p2];
-        let val_4 = self.values[p4];
+        let val_1 = grid.values[p1];
+        let val_2 = grid.values[p2];
+        let val_4 = grid.values[p4];
 
         let intersection_1 = pos_1.lerp(pos_2, val_2 / (val_1 + val_2));
         let intersection_2 = pos_1.lerp(pos_4, val_4 / (val_1 + val_4));
@@ -252,6 +278,7 @@ impl Grid {
 
     fn split(
         &self,
+        grid: &Grid,
         vertices: &mut Vec<[f32; 3]>,
         indices: &mut Vec<u32>,
         p1: usize,
@@ -259,15 +286,15 @@ impl Grid {
         p3: usize,
         p4: usize,
     ) {
-        let pos_1 = self.positions[p1];
-        let pos_2 = self.positions[p2];
-        let pos_3 = self.positions[p3];
-        let pos_4 = self.positions[p4];
+        let pos_1 = grid.positions[p1];
+        let pos_2 = grid.positions[p2];
+        let pos_3 = grid.positions[p3];
+        let pos_4 = grid.positions[p4];
 
-        let val_1 = self.values[p1];
-        let val_2 = self.values[p2];
-        let val_3 = self.values[p3];
-        let val_4 = self.values[p4];
+        let val_1 = grid.values[p1];
+        let val_2 = grid.values[p2];
+        let val_3 = grid.values[p3];
+        let val_4 = grid.values[p4];
 
         let intersection_1 = pos_1.lerp(pos_4, val_4 / (val_1 + val_4));
         let intersection_2 = pos_2.lerp(pos_3, val_3 / (val_2 + val_3));
@@ -293,6 +320,7 @@ impl Grid {
 
     fn diagonal(
         &self,
+        grid: &Grid,
         vertices: &mut Vec<[f32; 3]>,
         indices: &mut Vec<u32>,
         p1: usize,
@@ -300,15 +328,15 @@ impl Grid {
         p3: usize,
         p4: usize,
     ) {
-        let pos_1 = self.positions[p1];
-        let pos_2 = self.positions[p2];
-        let pos_3 = self.positions[p3];
-        let pos_4 = self.positions[p4];
+        let pos_1 = grid.positions[p1];
+        let pos_2 = grid.positions[p2];
+        let pos_3 = grid.positions[p3];
+        let pos_4 = grid.positions[p4];
 
-        let val_1 = self.values[p1];
-        let val_2 = self.values[p2];
-        let val_3 = self.values[p3];
-        let val_4 = self.values[p4];
+        let val_1 = grid.values[p1];
+        let val_2 = grid.values[p2];
+        let val_3 = grid.values[p3];
+        let val_4 = grid.values[p4];
 
         let intersection_1 = pos_1.lerp(pos_2, val_2 / (val_1 + val_2));
         let intersection_2 = pos_2.lerp(pos_3, val_3 / (val_2 + val_3));
@@ -336,6 +364,7 @@ impl Grid {
 
     fn square(
         &self,
+        grid: &Grid,
         vertices: &mut Vec<[f32; 3]>,
         indices: &mut Vec<u32>,
         p1: usize,
@@ -343,10 +372,10 @@ impl Grid {
         p3: usize,
         p4: usize,
     ) {
-        let pos_1 = self.positions[p1];
-        let pos_2 = self.positions[p2];
-        let pos_3 = self.positions[p3];
-        let pos_4 = self.positions[p4];
+        let pos_1 = grid.positions[p1];
+        let pos_2 = grid.positions[p2];
+        let pos_3 = grid.positions[p3];
+        let pos_4 = grid.positions[p4];
 
         vertices.extend([
             pos_1.as_ref(),
--- src/metaballs.rs
+++ src/metaballs.rs
@@ -1,49 +1,77 @@
 use bevy::prelude::*;
 
 pub struct Ball;
-pub struct Position{
-   pub pos: Vec2,
+pub struct Position {
+    pub pos: Vec2,
 }
-pub struct Radius{
+pub struct Radius {
     pub r: f32,
 }
-pub struct Veclocity{
+pub struct Veclocity {
     pub vel: Vec2,
 }
 
-pub fn setup(
-    mut commands: Commands,
-) {
-    commands.spawn()
+pub fn setup(mut commands: Commands) {
+    commands
+        .spawn()
         .insert(Ball)
-        .insert(Position { pos: Vec2::new(-200.0, 0.0) })
+        .insert(Position {
+            pos: Vec2::new(-200.0, 0.0),
+        })
         .insert(Radius { r: 30.0 })
-        .insert(Veclocity { vel: Vec2::new(1.0, 2.0) });
-    commands.spawn()
+        .insert(Veclocity {
+            vel: Vec2::new(1.0, 2.0),
+        });
+    commands
+        .spawn()
         .insert(Ball)
-        .insert(Position { pos: Vec2::new(200.0, 0.0) })
+        .insert(Position {
+            pos: Vec2::new(200.0, 0.0),
+        })
         .insert(Radius { r: 20.0 })
-        .insert(Veclocity { vel: Vec2::new(2.0, 4.0) });
-    commands.spawn()
-        .insert(Ball)
-        .insert(Position { pos: Vec2::new(20.0, 20.0) })
-        .insert(Radius { r: 10.0 })
-        .insert(Veclocity { vel: Vec2::new(3.0, 6.0) });
-    commands.spawn()
-        .insert(Ball)
-        .insert(Position { pos: Vec2::new(25.0, 25.0) })
-        .insert(Radius { r: 25.0 })
-        .insert(Veclocity { vel: Vec2::new(4.0, 3.0) });
-    commands.spawn()
-        .insert(Ball)
-        .insert(Position { pos: Vec2::new(22.0, 202.0) })
-        .insert(Radius { r: 22.0 })
-        .insert(Veclocity { vel: Vec2::new(5.0, -6.0) });
-    commands.spawn()
-        .insert(Ball)
-        .insert(Position { pos: Vec2::new(20.0, 20.0) })
-        .insert(Radius { r: 10.0 })
-        .insert(Veclocity { vel: Vec2::new(-3.0, 3.0) });
+        .insert(Veclocity {
+            vel: Vec2::new(2.0, 4.0),
+        });
+    // commands
+    //     .spawn()
+    //     .insert(Ball)
+    //     .insert(Position {
+    //         pos: Vec2::new(20.0, 20.0),
+    //     })
+    //     .insert(Radius { r: 10.0 })
+    //     .insert(Veclocity {
+    //         vel: Vec2::new(3.0, 6.0),
+    //     });
+    // commands
+    //     .spawn()
+    //     .insert(Ball)
+    //     .insert(Position {
+    //         pos: Vec2::new(25.0, 25.0),
+    //     })
+    //     .insert(Radius { r: 25.0 })
+    //     .insert(Veclocity {
+    //         vel: Vec2::new(4.0, 3.0),
+    //     });
+    // commands
+    //     .spawn()
+    //     .insert(Ball)
+    //     .insert(Position {
+    //         pos: Vec2::new(22.0, 202.0),
+    //     })
+    //     .insert(Radius { r: 22.0 })
+    //     .insert(Veclocity {
+    //         vel: Vec2::new(5.0, -6.0),
+    //     });
+    // commands
+    //     .spawn()
+    //     .insert(Ball)
+    //     .insert(Position {
+    //         pos: Vec2::new(20.0, 20.0),
+    //     })
+    //     .insert(Radius { r: 10.0 })
+    //     .insert(Veclocity {
+    //         vel: Vec2::new(-3.0, 3.0),
+    //     });
 }
 
 pub fn update_balls(mut q: Query<(&mut Position, &mut Veclocity), With<Ball>>) {
